e78894517a1a83a71f749593466261e3cd1d73d1
Jonathan Rockway <jon@jrock.us>
2007-05-05 05:04:25 -0500

    add database logger and tests

--- lib/App/Ircxory/Robot/DBLogger.pm
+++ lib/App/Ircxory/Robot/DBLogger.pm
@@ -5,6 +5,7 @@ use strict;
 use warnings;
 use Carp;
 use App::Ircxory::Config;
+use App::Ircxory::Robot::Parser;
 
 use base 'App::Ircxory::Schema';
 
@@ -37,12 +38,62 @@ sub connect {
     return $invocant->SUPER::connect($dsn, $user, $pass, $args);
 }
 
+=head2 record($action)
+
+Insert an action into the database, creating people, nicknames, and
+things if necessary.
+
+=cut
+
 sub record {
     my $self   = shift;
     my $action = shift;
 
-    warn "logging action: $action";
+    my $nickname = $self->get_nickname(parse_nickname($action->who));
+    my $thing    = $self->resultset('Things')->
+      find_or_create({ thing => $action->word });
+    
+    return $self->resultset('Opinions')->
+      create({ nickname  => $nickname,
+               thing     => $thing,
+               points    => $action->points,
+               message   => $action->message,
+               reason    => $action->reason,
+             });
 }
+
+=head2 get_nickname($nick, $user, $host)
+
+Given nick/username/host, try to find a nickname object that matches.
+If there isn't one, we'll create one and return that.
+
+If the nickname isn't associated with a Person object yet, one will
+be created.
+
+=cut
+
+sub get_nickname {
+    my $self = shift;
+    my ($nick, $user, $host) = @_;
+    
+    my $nickname = $self->resultset('Nicknames')->
+      find_or_create({ nick     => $nick, 
+                       username => $user,
+                       host     => $host,
+                     });
+
+    my $person = $nickname->person;
+    if (!$person) {
+        # TODO: be smarter about hostmasks etc.
+        my $person = $self->resultset('People')->
+          find_or_create({ name => $nick });
+        $nickname->person($person);
+        $nickname->update;
+    }
+    
+    return $nickname;
+}
+
 
 sub get_recorder {
     my $self = shift;
---
+++ t/irc-dblogger.t
@@ -0,0 +1,51 @@
+#!/usr/bin/env perl
+# Copyright (c) 2007 Jonathan Rockway <jrockway@cpan.org>
+
+use strict;
+use warnings;
+use Test::More tests => 11;
+use Directory::Scratch;
+use App::Ircxory::Schema;
+use App::Ircxory::Robot::DBLogger;
+use App::Ircxory::Robot::Action;
+
+my $tmp = Directory::Scratch->new;
+my $db  = $tmp->touch('database');
+
+my $schema = App::Ircxory::Schema->connect("DBI:SQLite:$db");
+$schema->deploy;
+bless $schema => 'App::Ircxory::Robot::DBLogger'; # ick.
+
+isa_ok($schema, 'App::Ircxory::Robot::DBLogger');
+
+my $person;
+{
+    my $nick = $schema->get_nickname('jrockway', 'jon', 'jrock.us');
+    is($nick->nick, 'jrockway');
+    ok($nick->person, 'have a person');
+    is($nick->person->name, 'jrockway', 'created a person with nick as name');
+    
+    my $old_person = $nick->person;
+    $nick = $schema->get_nickname('jrockway', 'jon', 'foo.jrock.us');
+    is($nick->nick, 'jrockway');
+    ok($nick->person, 'have a person');
+    is($nick->person->name, 'jrockway', 'created a person with nick as name');
+    is($nick->person->pid, $old_person->pid, 'same person as before');
+
+    $person = $nick->person; # for comparison later
+}
+
+my $action = App::Ircxory::Robot::Action->
+  new({ who       => 'jrockway!~jon@jrock.us',
+        message   => q{plusplus++ # it's fun},
+        word      => 'plusplus',
+        points    => 1,
+        reason    => q{it's fun},
+        channel   => '#plusplus',
+      });
+
+my $opinion = $schema->record($action);
+
+ok($opinion, 'inserted opinion record ok');
+is($opinion->nickname->person->pid, $person->pid, "opinion person is the same");
+is($opinion->thing->thing, 'plusplus');
