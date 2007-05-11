70400fe90b1a6ffc80381c65e43a51045e4fba74
Jonathan Rockway <jon@jrock.us>
2007-05-10 21:37:31 -0500

    add ability for bot to respond to "karma for" requests

---
+++ lib/App/Ircxory/Robot/Query/KarmaFor.pm
@@ -0,0 +1,17 @@
+# Copyright (c) 2007 Jonathan Rockway <jrockway@cpan.org>
+
+package App::Ircxory::Robot::Query::KarmaFor;
+use strict;
+use warnings;
+
+use base 'App::Ircxory::Robot::Query';
+
+__PACKAGE__->mk_accessors('target');
+
+=head1 NAME
+
+App::Ircxory::Robot::Query::KarmaFor - user wants to know the karma for something
+
+=cut
+
+1;
