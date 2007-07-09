2bc45ed74e8c597b4484b08b188e6d271c9572a2
Jonathan Rockway <jon@jrock.us>
2007-07-08 23:04:57 -0500

    add the one_thing display into TD

--- lib/App/Ircxory/Controller/Things.pm
+++ lib/App/Ircxory/Controller/Things.pm
@@ -8,26 +8,26 @@ use base 'Catalyst::Controller';
 
 sub all_things :Path Args(0) {
     my ($self, $c) = @_;
-    $c->stash(template   => 'things.tt2');
-    $c->stash(everything => [
-                             $c->model('DBIC::Things')->
-                               everything({},{ order_by => 
-                                               'SUM(opinions.points) DESC'})
-                            ]);
+    $c->stash(everything => 
+              scalar $c->model('DBIC::Things')->
+                  everything({},
+                             { order_by => 'SUM(opinions.points) DESC' }
+                            )
+             );
 }
 
 sub one_thing :Path Args(1) {
     my ($self, $c, $thing) = @_;
     my $m = $c->model('DBIC');
-
+    
     # aggregates
-    $c->stash(template => 'thing.tt2');
     $c->stash(thing    => $thing);
     $c->stash(points   => $m->karma_for($thing));
     $c->stash(ups      => $m->karma_for($thing, 1));
     $c->stash(downs    => $m->karma_for($thing, -1));
-    $c->stash(person   => $m->resultset('People')->
-              find({ name => $thing}, { key => 'nickname' })
+    $c->stash(person   => 
+              scalar $m->resultset('People')->find({ name => $thing},
+                                                   { key => 'nickname' })
              );
     
     # detailed reasons
--- lib/App/Ircxory/View/TD/Things.pm
+++ lib/App/Ircxory/View/TD/Things.pm
@@ -4,6 +4,8 @@ package App::Ircxory::View::TD::Things;
 use strict;
 use warnings;
 use App::Ircxory::View::TD::Wrapper;
+use App::Ircxory::View::TD::People;
+use App::Ircxory::View::TD::Pair;
 
 use base 'Exporter';
 our @EXPORT = qw/list_things thing score/;
@@ -49,5 +51,74 @@ sub controversy_list_things($) {
         }
     }
 }
+
+sub list_reasons {
+    my $opinions_ref = shift;
+    if (!$opinions_ref->[0]){
+        p { 'None!' };
+        return;
+    }
+
+    foreach my $opinion (@$opinions_ref){
+        div { attr { class => 'reason' };
+              p {
+                  span { attr { class => 'opinion_reason' };
+                         $opinion->reason;
+                     };
+                  span { attr { class => 'written_by' };
+                         person { $opinion->person };
+                     };
+              };
+          };
+    }
+}
+
+template 'things/one_thing' => sub {
+    my $thing  = c->stash->{thing};
+    my $points = c->stash->{points};
+    my $ups    = c->stash->{ups};
+    my $downs  = c->stash->{downs};
+    
+    wrapper {
+        h2 { "Information on $thing" };
+        p { 
+            outs("$thing has karma of ");
+            score { $points };
+            outs(" from being upmodded ");
+            score { $ups };
+            outs(" times and downmodded ");
+            span { attr { class => 'negative_score' };
+                   $downs 
+               };
+            outs(" times.");
+        };
+        if (c->stash->{person}) {
+            p {
+                person { c->stash->{person}->name };
+                outs(' is also a person!');
+            };
+        }
+
+        pair( left_title  => "Reasons why $thing is loved",
+              right_title => "Reasons why $thing is hated",
+              left        => sub { list_reasons(c->stash->{up_reasons}) },
+              right       => sub { list_reasons(c->stash->{down_reasons}) },
+              width       => '50em',
+            );
+        div { attr { class => 'pair', style => 'width: 50em' };
+              div { attr { class => 'box', 
+                           style => 'padding-left: 13em; padding-right: 13em;',
+                       };
+                    my $neutral_reasons = c->stash->{neutral_reasons};
+                    if ($neutral_reasons->[0]) {
+                        h3 {
+                            outs("Reasons why $thing is meh");
+                            list_reasons($neutral_reasons);
+                        }
+                    }
+                }
+          };
+    };
+};
 
 1;
