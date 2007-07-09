7c19bad690ca9ae150e8b5366e7dda6c53aac5b8
Jonathan Rockway <jon@jrock.us>
2007-07-08 19:53:00 -0500

    add TD view, kill TT stuff

---
+++ lib/App/Ircxory/View/TD.pm
@@ -0,0 +1,43 @@
+package App::Ircxory::View::TD;
+use strict;
+use warnings;
+
+use base 'Catalyst::View::Template::Declare';
+use Template::Declare::Tags;
+
+
+# Test Template, replace with your own
+# template test => sub {
+#     html {
+#         head {
+#             title {"This is a test" }
+#         };
+#         body {
+#             h1 { "This is a test" };
+#             p { "Welcome to Template::Declare." }
+#         }
+#     }
+# };
+#
+# You can also put templates in separate modules, each under the
+# App::Ircxory::View::TD::* prefix.
+  
+=head1 NAME
+
+App::Ircxory::View::TD - Template::Declare View for App::Ircxory
+
+=head1 DESCRIPTION
+
+Template::Declare View for App::Ircxory. 
+
+=head1 AUTHOR
+
+Jonathan Rockway,,,
+
+=head1 SEE ALSO
+
+L<App::Ircxory>
+
+=cut
+
+1;
