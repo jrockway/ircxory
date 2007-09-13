d9d43674140a8a81127e0bad851cc54bf4e21a5b
Jonathan Rockway <jon@jrock.us>
2007-09-13 01:59:02 -0500

    remove blank space from top of doc

--- lib/App/Ircxory/View/TD.pm
+++ lib/App/Ircxory/View/TD.pm
@@ -4,6 +4,15 @@ use warnings;
 
 use base 'Catalyst::View::Template::Declare';
 
+sub process {
+    my $self = shift;
+    my $c    = shift;
+    $self->SUPER::process($c, @_);
+    $c->res->{body} =~ s/^\n+//;
+}
+  
+
+
 =head1 NAME
 
 App::Ircxory::View::TD - Template::Declare View for App::Ircxory
